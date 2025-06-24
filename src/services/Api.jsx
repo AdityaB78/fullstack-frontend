85bea902457b7ecf73262ea2389885b3e51b8f03
Aditya Bhalerao <126479299+AdityaB78@users.noreply.github.com>
2025-06-24 10:22:30 +0530

    Update Api.jsx

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://fullstack-backend-gqj3.vercel.app/api ',
+  baseURL: 'http://localhost:5000/api',
   timeout: 5000,
 });
 
